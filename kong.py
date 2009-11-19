fe313583b8ddbb69ed84aac4c1291c0a984b868e
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-11-18 23:22:09 -0800

    kong: Cleanup dict class.
    That's right, I'm using overly-clever __getattr__ on the Internets!
    HA HA!

--- kong.py
+++ kong.py
@@ -28,39 +28,51 @@ def print_stats(user_badges, total_badges):
     user_count = len(user_badges)
     total_count = len(total_badges)
 
-    # XXX replace with LC if it's faster, maybe
-    user_imp_count, imp_count = 0, 0
-    for imp in total_badges.iter_by_difficulty("impossible"):
-        imp_count += 1
-        if imp["id"] in user_badges:
-            user_imp_count += 1
-
     print_percentage("Acquired Badges", user_count, total_count)
+
+    imp_count = total_badges.count_by_difficulty()["impossible"]
+    user_imp_count = sum(1 for imp in
+        total_badges.iter_by_difficulty("impossible")
+        if imp["id"] in user_badges)
+
     print_percentage("Impossibles", user_imp_count, imp_count)
 
-class BadgeDict(UserDict.IterableUserDict):
+class BadgeDict(dict):
     def __init__(self, iterable=[]):
-        # XXX IterableUserDict isn't new-style ( >&)
-        UserDict.IterableUserDict.__init__(self)
+        super(BadgeDict, self).__init__()
 
         for entry in iterable:
             self[entry["id"]] = entry
 
-    def count_by_difficulty(self):
-        # XXX missing_method-style mangling maybe?
-        d = collections.defaultdict(int)
-        for entry in self.itervalues():
-            d[entry["difficulty"]] += 1
-        return d
+    def __getattr__(self, name):
+        type, chaff, target = name.partition("_by_")
+        if not target:
+            raise AttributeError
 
-    def iter_by_difficulty(self, difficulty):
-        # XXX mm?
-        for entry in self.itervalues():
-            if entry["difficulty"] == difficulty:
-                yield entry
+        if type == "count":
+            def f(self):
+                d = collections.defaultdict(int)
+                for entry in self.itervalues():
+                    d[entry[target]] += 1
+                return d
+
+            setattr(self.__class__, name, f)
+        elif type == "iter":
+            def f(self, filter):
+                for entry in self.itervalues():
+                    if entry[target] == filter:
+                        yield entry
+            setattr(self.__class__, name, f)
+
+        return getattr(self, name)
 
 badges = BadgeDict(json.load(file("badges.json", "r")))
 madbadges = set(i["badge_id"] for i in
     json.load(file("MostAwesomeDude.json", "r")))
 
 print_stats(madbadges, badges)
+
+# XXX debugging
+for i in badges.itervalues():
+    pprint.pprint(i)
+    break
