e5894b5951ad93daa4d530ad204d3987b1d60fc1
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-11-18 22:49:17 -0800

    kong: Add Imp stats.

--- kong.py
+++ kong.py
@@ -3,12 +3,40 @@
 
 from __future__ import division
 
+import collections
 import json
 import UserDict
 
-def print_stats(user_count, total_count):
-    print "Acquired Badges: %d of %d (%2.2f%% complete)" % (
-        user_count, total_count, (user_count * 100) / total_count)
+import pprint
+
+def print_percentage(label, current, total):
+    """
+    Pretty-print a percentage in a uniform manner.
+    """
+
+    print "%s: %d of %d (%2.2f%% complete)" % (label, current, total,
+        (current * 100) / total)
+
+def print_stats(user_badges, total_badges):
+    """
+    Print badge statistics.
+
+    user_badges is an iterable of ints.
+    total_badges is a `BadgeDict`.
+    """
+
+    user_count = len(user_badges)
+    total_count = len(total_badges)
+
+    # XXX replace with LC if it's faster, maybe
+    user_imp_count, imp_count = 0, 0
+    for imp in total_badges.iter_by_difficulty("impossible"):
+        imp_count += 1
+        if imp["id"] in user_badges:
+            user_imp_count += 1
+
+    print_percentage("Acquired Badges", user_count, total_count)
+    print_percentage("Impossibles", user_imp_count, imp_count)
 
 class BadgeDict(UserDict.IterableUserDict):
     def __init__(self, iterable=[]):
@@ -16,12 +44,23 @@ class BadgeDict(UserDict.IterableUserDict):
         UserDict.IterableUserDict.__init__(self)
 
         for entry in iterable:
-            if "id" in entry:
-                self[entry["id"]] = entry
-            elif "badge_id" in entry:
-                self[entry["badge_id"]] = entry
+            self[entry["id"]] = entry
+
+    def count_by_difficulty(self):
+        # XXX missing_method-style mangling maybe?
+        d = collections.defaultdict(int)
+        for entry in self.itervalues():
+            d[entry["difficulty"]] += 1
+        return d
+
+    def iter_by_difficulty(self, difficulty):
+        # XXX mm?
+        for entry in self.itervalues():
+            if entry["difficulty"] == difficulty:
+                yield entry
 
 badges = BadgeDict(json.load(file("badges.json", "r")))
-madbadges = BadgeDict(json.load(file("MostAwesomeDude.json", "r")))
+madbadges = set(i["badge_id"] for i in
+    json.load(file("MostAwesomeDude.json", "r")))
 
-print_stats(len(madbadges), len(badges))
+print_stats(madbadges, badges)
